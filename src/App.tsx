83db37f2afe9d9a2014fe0e94d395be65832abf9
Michael Clapham <michaeljclapham@gmail.com>
2019-09-15 18:30:40 +0100

    Add QR Code Reader

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,44 @@ import React from 'react';
 import logo_fg from './logo_fg1.svg';
 import logo_bg from './sync_arrows1.svg';
 import './App.css';
+import QrReader from 'react-qr-reader';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="app-title-container">
-          <div className="app-logo-container">
-            <img src={logo_fg} className="app-logo-fg" alt="logo" />
-            <img src={logo_bg} className="app-logo-bg" alt="logo-bg" />
+export default class App extends React.Component<{}, { result: string }> {
+
+  state = {} as any;
+
+  handleScan = (result: string | null) => {
+    if (result) {
+      this.setState({ result: result });
+    } else {
+      this.setState({ result: "No Result"});
+    }
+  }
+
+  handleError = () => {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="app-title-container">
+            <div className="app-logo-container">
+              <img src={logo_fg} className="app-logo-fg" alt="logo" />
+              <img src={logo_bg} className="app-logo-bg" alt="logo-bg" />
+            </div>
+            <h1 className="app-title">QR Sync</h1>
           </div>
-          <h1 className="app-title">QR Sync</h1>
-        </div>
-      </header>
-    </div>
-  );
+        </header>
+        <QrReader
+          delay={300}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '60vmin', margin: 'auto', position: 'relative', marginTop: 30 }}
+        ></QrReader>
+        <p>Result: {this.state.result}</p>
+      </div>
+    );
+  }
 }
-
-export default App;
