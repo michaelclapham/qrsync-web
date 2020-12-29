c24735241bceeb385843600b0a4e23323a6e1c89
Michael Clapham <michaeljclapham@gmail.com>
2020-12-29 11:42:01 +0000

    Gets own id from server msg

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import logo_fg from './logo_fg1.svg';
 import logo_bg from './sync_arrows1.svg';
 import './App.css';
-import { Client } from './Client';
 import { IonButton, IonModal } from '@ionic/react';
 import { ScanPeerModal } from './feature/scan-peer/ScanPeerModal';
 import QRCode from "react-qr-code";
+import { ServerTypes } from './ServerTypes';
+import { Client } from './Client';
 
 interface AppState {
   result: string;
   goToURL: string;
   clientMap: Record<string, Client>;
   scanModalOpen: boolean;
+  ourClientId: string;
 }
 
 export default class App extends React.Component<{}, AppState> {
@@ -22,13 +24,38 @@ export default class App extends React.Component<{}, AppState> {
       result: "",
       goToURL: "",
       clientMap: {},
-      scanModalOpen: false
+      scanModalOpen: false,
+      ourClientId: ""
     };
+    this.connectToWebsocket();
   }
 
   handleUrlSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 
   };
+
+  connectToWebsocket = () => {
+    const ws = new WebSocket("ws://localhost:4001/v1/ws");
+    ws.onmessage = (event) => {
+      console.log("ws event", event);
+      const msg: ServerTypes.Msg = JSON.parse(event.data);
+      this.onReceiveWebsocketMsg(msg);
+    }
+  }
+
+  onReceiveWebsocketMsg = (msg: ServerTypes.Msg) => {
+    if (msg.type) {
+      switch (msg.type) {
+        case "ClientConnect": return this.onClientConnectMsg(msg)
+      }
+    } else {
+      console.warn("No message type ", msg);
+    }
+  }
+
+  onClientConnectMsg = (msg: ServerTypes.ClientConnectMsg) => {
+    this.setState({ ourClientId: msg.client.id });
+  }
 
   onScanPeer = (client: Client) => {
     this.setState({ clientMap: { ...this.state.clientMap, client } })
@@ -48,7 +75,7 @@ export default class App extends React.Component<{}, AppState> {
             <h1 className="app-title">QR Sync</h1>
           </div>
         </header>
-        <QRCode value="bleh"></QRCode>
+        { this.state.ourClientId.length > 0 ? <QRCode value={this.state.ourClientId}></QRCode> : null }
         <p>Result: {this.state.result}</p>
         <form onSubmit={this.handleUrlSubmit}>
           <label>
--- src/Client.ts
+++ src/Client.ts
@@ -1,6 +1,5 @@
 export interface Client {
-    qr: string;
     id: string;
     name: string;
-    gotoUrl: string;
+    remoteAddr: string;
 };
